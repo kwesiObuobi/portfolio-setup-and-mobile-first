9d7ee3b5a9f46b62428817c2f8a83e6799ab0ea5
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-03-08 11:01:32 +0000

    Add closeModal() function to close the modal on large screens

--- script.js
+++ script.js
@@ -18,48 +18,48 @@ const projects = [
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage: "assets/img-placeholder.png",
         technologies: ['Ruby on rails', 'css', 'Javascript', 'html'],
-        liveVersion: "#",
-        sourceLink: "#"
+        liveVersion: "https://kwesiobuobi.github.io/portfolio-setup-and-mobile-first/",
+        sourceLink: "https://github.com/kwesiObuobi/portfolio-setup-and-mobile-first"
     },
     {
         name: "Multi-Post Stories <br> Gain+Glory2",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage: "assets/img-placeholder.png",
         technologies: ['Ruby on rails', 'css', 'Javascript', 'html'],
-        liveVersion: "#",
-        sourceLink: "#"
+        liveVersion: "https://kwesiobuobi.github.io/portfolio-setup-and-mobile-first/",
+        sourceLink: "https://github.com/kwesiObuobi/portfolio-setup-and-mobile-first"
     },
     {
         name: "Multi-Post Stories <br> Gain+Glory3",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage: "assets/img-placeholder.png",
         technologies: ['Ruby on rails', 'css', 'Javascript', 'html'],
-        liveVersion: "#",
-        sourceLink: "#"
+        liveVersion: "https://kwesiobuobi.github.io/portfolio-setup-and-mobile-first/",
+        sourceLink: "https://github.com/kwesiObuobi/portfolio-setup-and-mobile-first"
     },
     {
         name: "Multi-Post Stories <br> Gain+Glory4",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage: "assets/img-placeholder.png",
         technologies: ['Ruby on rails', 'css', 'Javascript', 'html'],
-        liveVersion: "#",
-        sourceLink: "#"
+        liveVersion: "https://kwesiobuobi.github.io/portfolio-setup-and-mobile-first/",
+        sourceLink: "https://github.com/kwesiObuobi/portfolio-setup-and-mobile-first"
     },
     {
         name: "Multi-Post Stories <br> Gain+Glory5",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage: "assets/img-placeholder.png",
         technologies: ['Ruby on rails', 'css', 'Javascript', 'html'],
-        liveVersion: "#",
-        sourceLink: "#"
+        liveVersion: "https://kwesiobuobi.github.io/portfolio-setup-and-mobile-first/",
+        sourceLink: "https://github.com/kwesiObuobi/portfolio-setup-and-mobile-first"
     },
     {
         name: "Multi-Post Stories <br> Gain+Glory6",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
         featuredImage: "assets/img-placeholder.png",
         technologies: ['Ruby on rails', 'css', 'Javascript', 'html'],
-        liveVersion: "#",
-        sourceLink: "#"
+        liveVersion: "https://kwesiobuobi.github.io/portfolio-setup-and-mobile-first/",
+        sourceLink: "https://github.com/kwesiObuobi/portfolio-setup-and-mobile-first"
     }
 ];
 
@@ -101,32 +101,49 @@ for (let i = 0; i < projects.length; i++) {
     projectBtn.innerHTML = "See Project";
 
     projectBtn.addEventListener('click', function() {
-        console.log('reached');
         document.querySelector('.modal-container').style.display = 'flex';
         document.body.style.overflow = 'hidden';
         document.querySelector('.modal-container').innerHTML = `
-        <div class="modal-img-box">
-            <img src="assets/modal-image-mobile.png" alt="project snapshot" class="modal-featured-img">
-            <img src="assets/modal-close.png" alt="close button" class="modal-close-btn" onclick="closeMe()">
+        <div class="modal-content">
+            <img src="assets/close-desktop-modal.png" alt="close button" class="close-desktop-modal" onclick="closeModal()">
+            <div class="modal-img-box">
+                <img src="assets/modal-image-mobile.png" alt="project snapshot" class="modal-featured-img">
+                <img src="assets/modal-close.png" alt="close button" class="modal-close-btn" onclick="closeModal()">
+            </div>
+            <div class="modal-h3-skills-and-btns">
+                <div class="modal-h3-and-skills">
+                    <h3 class="modal-project-title">${projects[i].name}</h3>
+                    <ul class="modal-tags">
+                        <li class="modal-tag">Ruby on Rails</li>
+                        <li class="modal-tag">css</li>
+                        <li class="modal-tag">html</li>
+                        <li class="modal-tag">Javascript</li>
+                    </ul>
+                </div>
+                <ul class="modal-top-btn-group">
+                    <li class="btn top-modal-btn">
+                        <a href=${projects[i].liveVersion} class="">See Live</a>
+                        <img src="assets/see-live.png" alt="see live icon" class="modal-top-btn-icon">
+                    </li>
+                    <li class="btn top-modal-btn">
+                        <a href=${projects[i].sourceLink} class="">See Source</a>
+                        <img src="assets/modal-github.png" alt="github icon" class="modal-top-btn-icon">
+                    </li>
+                </ul>
+            </div>
+            
+            <p class="modal-primary-text">${projects[i].description}</p>
+            <ul class="modal-btn-group">
+                <li class="btn modal-btn">
+                    <a href=${projects[i].liveVersion} class="">See Live</a>
+                    <img src="assets/see-live.png" alt="see live icon">
+                </li>
+                <li class="btn modal-btn">
+                    <a href=${projects[i].sourceLink} class="">See Source</a>
+                    <img src="assets/modal-github.png" alt="github icon">
+                </li>
+            </ul>
         </div>
-        <h3 class="modal-project-title">keeping track of hundreds of components</h3>
-        <ul class="modal-tags">
-            <li class="modal-tag">assets/modal-image-mobile.png}</li>
-            <li class="modal-tag">${projects[i].technologies[1]}</li>
-            <li class="modal-tag">${projects[i].technologies[2]}</li>
-            <li class="modal-tag">${projects[i].technologies[3]}</li>
-        </ul>
-        <p class="modal-primary-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
-        <ul class="modal-btn-group">
-            <li class="btn modal-btn">
-                <a href="#" class="">See Live</a>
-                <img src="assets/see-live.png" alt="see live icon">
-            </li>
-            <li class="btn modal-btn">
-                <a href="#" class="">See Source</a>
-                <img src="assets/modal-github.png" alt="github icon">
-            </li>
-        </ul>
         `;
     });
 
@@ -138,3 +155,10 @@ for (let i = 0; i < projects.length; i++) {
     recentWorksGroup.appendChild(recentWorksItem);
     recentWorksSection.appendChild(recentWorksGroup);
 }
+
+function closeModal() {
+    document.body.style.overflow = 'scroll';
+  document.querySelector('.modal-container').style.display = 'none';
+}
+
+closeModal();
